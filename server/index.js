c0f83fce8c4eb0cb384e08392a706f7e16d5e6d8
Zaliant1 <ATMendoza1092@gmail.com>
2022-10-30 23:51:44 -0600

    deploying

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-
+app.use("/js", express.static(path.join(__dirname, "js")));
 app.listen(4000, () => console.log("up on 4000"));
 
 app.get("/", (req, res) => {
